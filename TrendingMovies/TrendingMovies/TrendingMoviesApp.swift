0b37110928847e917b8609e8e1c052a9b7c718f7
MG <manishjpgupta@gmail.com>
2024-09-16 15:45:13 -0400

    Adding initial setup for the app

---
+++ TrendingMovies/TrendingMovies/TrendingMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingMoviesApp.swift
+//  TrendingMovies
+//
+//  Created by Manish Gupta on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TrendingMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
